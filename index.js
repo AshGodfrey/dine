5faa2f8891feed811f816a9d43200a46ed00e52a
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-01-12 10:17:53 -0500

    added basic user input functionality

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-//api key
-const apiKey = "fbf613818dmsh4d46ff50d583636p1e4b42jsn8c77729e63e4";
 
 //display results
 function displayRandom(result) {
-	console.log(result.recipes[0].image);
+	console.log(result.recipes[0]);
+	$('.results').removeClass('hidden');
+	$('#random-results').append(`<img src="${result.recipes[0].image}" class="results-img">`)
 }
+
+function displaySearch() {
+	//display primary search
+}
+
+
+
+
 
 //perform searches
 function searchRandom() {
+	let userValue = $("#random-recipe").val();
 	$.ajax({ 
    type : "GET", 
    dataType: "json",
-   url : "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=1&tags=vegetarian%2Cdessert", 
+   url : `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=1&tags=${userValue}`, 
    beforeSend: function(xhr){xhr.setRequestHeader('X-RapidAPI-Key', 'fbf613818dmsh4d46ff50d583636p1e4b42jsn8c77729e63e4');},
    success : function(result) { 
        displayRandom(result);
@@ -22,11 +31,22 @@ function searchRandom() {
 });
 }
 
+function searchStandard() {
+	//see above for a guide
+}
+
+
+
+
+
+
+
 //Search Click Events
 function submitRandomForm() {
 	$('#random-search').submit(event => {
     event.preventDefault();
     searchRandom();
+    $('#random-results').empty();
   });
 }
 
@@ -36,6 +56,13 @@ function submitSearch() {
     alert("clicked");
   });
 }
+
+
+
+
+
+
+
 
 //Run the app
 submitRandomForm();
